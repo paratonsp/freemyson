47dde1a357e8000e4e3bd91c4b9e3a2e94de3ae8
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-07 15:31:18 +0700

    Create p-store.lua

---
+++ p-store.lua
@@ -0,0 +1,77 @@
+div       = 750000
+base      = balance/div
+nextbet   = base
+bethigh   = false
+p         = 0
+l         = 0
+wincount  = 0
+losecount = 0
+a         = 0
+t         = 5
+po        = 1
+multi     = 1.17
+chance    = 91
+
+resetstats()
+function dobet()
+
+if wincount == 5 then
+    resetseed()  
+    bethigh=!bethigh
+    wincount = 0
+end
+
+if currentprofit > 0 then
+    p+= currentprofit
+ else
+    l-= currentprofit
+end 
+
+if win then
+    base    = balance/div
+    nextbet = base
+    chance  = math.random(3000,8000)/100
+    wincount+=1
+    p =0
+    l = 0
+    a = 0
+    losecount = 0
+ else
+    a = math.random(4,9)
+    nextbet = l/po
+    
+    if a == 4 then
+        chance = 58.6
+        po = 0.70477
+        nextbet = (l+base)/po
+    end
+    if a == 5 then
+        chance = 59.6
+        po = 0.6761
+        nextbet = (l+base)/po
+    end
+    if a == 6 then
+        chance = 60.6
+        po = 0.6485
+        nextbet = (l+base)/po
+    end
+    if a == 7 then
+        chance = 61.8
+        po = 0.6165
+        nextbet = (l+base)/po
+    end
+    if a == 8 then
+        chance = 62.68 
+        po = 0.593
+        nextbet = (l+base)/po
+    end 
+    if a == 9 then
+        chance = 63.68
+        po = 0.5687
+        nextbet = (l+base)/po
+    end
+    losecount+=1
+end
+profitn=profit/(balance-profit)*100
+print("\nProfit: "..string.format("%.5f",profit).." "..currency.." ("..string.format("%.2f",profitn).."%)\n")
+end
