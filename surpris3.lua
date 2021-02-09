ad98dff4e3e7c4393bfe7cf1078c87b1ba393b7f
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-10 00:54:07 +0700

    Update surpris3.lua

--- surpris3.lua
+++ surpris3.lua
@@ -1,27 +1,16 @@
---botspeed=1
 nextbet=0.0
 chance = 98
 bethigh=true
 b=0
 resetstats()
-resetseed()
 function dobet()
-b+=1
-if (b>=60) then
-print(" ")
-resetseed()
-bethigh=!bethigh
-b=0
-end
 if win then
 nextbet=0.0
 else
 nextbet=0.0
-if (currentstreak==-1) then
-nextbet=balance end end
+if (currentstreak==-3) then
+nextbet=balance; bethigh=!bethigh; end end
 profitn=profit/(balance-profit)*100
-print("\n"..string.format("%.2f",balance)
-..currency.."/"..string.format("%.2f",profit)
-..currency.."("..string.format("%.2f",profitn)
+print("\n("..string.format("%.2f",profitn)
 .."%)\n")
 end
