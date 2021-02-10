a7d98290624486198d323f4c7c5c52cc75c79b9f
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-10 23:21:19 +0700

    Update unXpected.lua

--- unXpected.lua
+++ unXpected.lua
@@ -1,45 +1,70 @@
---
 nextbet=0.0
 chance=98
+sseed=25000
+maxstreakloses=0
+streakloses=0
 resetstats()
 resetseed()
 --
 function dobet()
+--
+worststreak()
+--
 if win then
-    nextbet=0.0
-    print("SCANNING SEED!!! || Bets: "..bets)
+if (lastBet.nonce>=sseed) then
+  nextbet=0.0
 else
-    nextbet=0.0
-    print("SCANNING SEED!!! || Bets: "..bets)
+nextbet=0.0
+print("SCANNING SEED || Nonce: "..lastBet.nonce)
+end
 end
 --
-if win and (bets>=25000) then
-    nextbet=0.0
-end
---
-if !win and (bets>=25000) then
--- WORSTSTREAK==-1
-if (worststreak==-1) then
+if !win then
+if (lastBet.nonce>=sseed) then
+-- WORSTSTREAK==1
+if (maxstreakloses==1) then
     if (currentstreak==-1) then
-    nextbet=balance/1
+    nextbet=balance/1.01
+    print("\n"..string.format("%.2f",profit)..currency
+    .."("..string.format("%.2f",profitn).."%)\n")
     end
 end
--- WORSTSTREAK==-2
-if (worststreak==-2) then
+-- WORSTSTREAK==2
+if (maxstreakloses==2) then
     if (currentstreak==-1) then
     nextbet=balance/100
-    end
-    if (currentstreak==-2) then
-    nextbet=previousbet*99
-    end
-end
--- WORSTSTREAK==-3
-if (worststreak==-3) then
-    stop()
-    print("NOT PROFITABLE")
-end
-print("\n"..string.format("%.2f",profit)..currency
+    print("\n"..string.format("%.2f",profit)..currency
     .."("..string.format("%.2f",profitn).."%)\n")
+    elseif (currentstreak==-2) then
+    nextbet=previousbet*99
+    print("\n"..string.format("%.2f",profit)..currency
+    .."("..string.format("%.2f",profitn).."%)\n")
+    end
 end
-if (bets>=1000000) then stop() end
+else
+-- WORSTSTREAK==3
+if (maxstreakloses==3) then
+    resetstats()
+    resetseed()
 end
+nextbet=0.0
+print("SCANNING SEED || Nonce: "..lastBet.nonce)
+end
+end
+if (lastBet.nonce>=999999) then
+    resetstats()
+    resetseed()
+end
+end
+--
+function worststreak()
+	if !win then
+		streakloses=streakloses+1
+		if maxstreakloses<=streakloses then
+			maxstreakloses=streakloses
+		end
+	else
+		streakloses=0
+	end
+end
+--
