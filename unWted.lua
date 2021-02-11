bf775666f1be244f0067a9e02d49db5a047e730f
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-11 12:58:37 +0700

    Update unWted.lua

--- unWted.lua
+++ unWted.lua
@@ -1,58 +1,75 @@
---
-adiv=9.9
-nextbet=0.0
-chance = 98
-bethigh=true
-bet=0
+nextbet        = 0.0
+chance         = 98
+sseed          = 100
+mseed          = 125
+maxstreakloses = 0
+streakloses    = 0
+bethigh        = true
 resetstats()
 resetseed()
 --
 function dobet()
-bet+=1
+--
+if (lastBet.nonce>0) and (lastBet.nonce<=sseed) then
+worststreak()
+end
+--
 if win then
---
-if (bet==25000) then
-resetseed()
-bethigh=!bethigh
-bet=0
-end
---
-div=1000000
-a=div*adiv
-base=balance/a
-nextbet=base
-chance=0.01
+if (lastBet.nonce>=sseed) then
+  nextbet=0.0
 else
-multi=1.03
-if (currentstreak==-1) then nextbet=previousbet*multi; chance=0.09; end
-if (currentstreak==-2) then nextbet=previousbet*multi; chance=0.19; end
-if (currentstreak==-3) then nextbet=previousbet*multi; chance=0.29; end
-if (currentstreak==-4) then nextbet=previousbet*multi; chance=0.39; end
-if (currentstreak==-5) then nextbet=previousbet*multi; chance=0.49; end
-if (currentstreak==-6) then nextbet=previousbet*multi; chance=0.59; end
-if (currentstreak==-7) then nextbet=previousbet*multi; chance=0.69; end
-if (currentstreak==-8) then nextbet=previousbet*multi; chance=0.79; end
-if (currentstreak==-9) then nextbet=previousbet*multi; chance=0.89; end
-if (currentstreak==-10) then nextbet=previousbet*multi; chance=0.99; end
-if (currentstreak==-11) then nextbet=previousbet*multi; chance=1.09; end
-if (currentstreak==-12) then nextbet=previousbet*multi; chance=1.19; end
-if (currentstreak==-13) then nextbet=previousbet*multi; chance=1.29; end
-if (currentstreak==-14) then nextbet=previousbet*multi; chance=1.39; end
-if (currentstreak==-15) then nextbet=previousbet*multi; chance=1.49; end
-if (currentstreak==-16) then nextbet=previousbet*multi; chance=1.59; end
-if (currentstreak==-17) then nextbet=previousbet*multi; chance=1.69; end
-if (currentstreak==-18) then nextbet=previousbet*multi; chance=1.79; end
-if (currentstreak==-19) then nextbet=previousbet*multi; chance=1.89; end
-if (currentstreak==-20) then nextbet=previousbet*multi; chance=1.99; end
-if (currentstreak==-21) then nextbet=previousbet*multi; chance=2.09; end
-if (currentstreak==-22) then nextbet=previousbet*multi; chance=2.19; end
-if (currentstreak==-23) then nextbet=previousbet*multi; chance=2.29; end
-if (currentstreak==-24) then nextbet=previousbet*multi; chance=2.39; end
-if (currentstreak==-25) then nextbet=previousbet*multi; chance=2.49; end
-if (currentstreak<=-26) then nextbet=previousbet*multi; chance=2.49; end
-if (currentstreak==-425) then stop(); print("MAX STREAK")end
+nextbet=0.0
+print("SCANNING SEED || Nonce: "..lastBet.nonce)
+end
 end
 --
-profitn=profit/(balance-profit)*100
-print("\n"..string.format("%.2f",balance)..currency.."|"..string.format("%.2f",profit)..currency.." ("..string.format("%.2f",profitn).."%)\n")
+if !win then
+if (lastBet.nonce>=sseed) then
+-- WORSTSTREAK==1
+if (maxstreakloses==1) then
+    if (currentstreak==-1) then
+    nextbet = balance/1
+    end
 end
+profitn=profit/(balance-profit)*100
+print("\n"..string.format("%.2f",profit)..currency
+.."("..string.format("%.2f",profitn).."%)\n")
+else
+-- WORSTSTREAK>2
+if (maxstreakloses>=2) then
+    resetstats()
+    resetseed()
+    maxstreakloses=0
+    streakloses=0
+    bethigh=!bethigh
+end
+nextbet=0.0
+print("SCANNING SEED || Nonce: "..lastBet.nonce)
+end
+end
+--
+if (lastBet.nonce>=mseed) then
+    resetstats()
+    resetseed()
+    maxstreakloses = 0
+    streakloses    = 0
+    bethigh        = !bethigh
+end
+if (lastBet.nonce>=(mseed+1)) then
+    stop() end
+if (balance==0.0000000) then
+    stop() end
+--
+end
+--
+function worststreak()
+  if !win then
+    streakloses=streakloses+1
+    if maxstreakloses<=streakloses then
+      maxstreakloses=streakloses
+    end
+  else
+    streakloses=0
+  end
+end
+--
