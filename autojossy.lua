dbee53fe3b9d88049e1b0d7647bbf9edd7cd5c7e
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-17 02:59:55 +0700

    Update autojossy.lua

--- autojossy.lua
+++ autojossy.lua
@@ -1,37 +1,70 @@
--------------------------------------------
--- 7 max losses (for 6 losestreak) - 27.364138
--- 8 max losses (for 7 losestreak) - 7.3446286
--- 9 max losses (for 8 losestreak) - 1.9714477
--- 10 max losses (for 9 losestreak) - 0.5291766
--- 11 max losses (for 10 losestreak) - 0.1420417
--- 12 max losses (for 11 losestreak) - 0.03812690
--- 13 max losses (for 12 losestreak) - 0.01023403
------------
-betcalc = 0.01023403
--------------------------------------------
 bethigh = true
 stopnow = false
 first = true
 done = true
 chance = 27.5
+betcalc = 0.01023403
 gan = 100000
 base = balance * (betcalc / gan)
 curbet = base * 3.7255
 nextbet = base
-maxbets=50000
+maxstreakloses = 0
+streakloses = 0
 resetseed()
-
+resetstats()
+--
 function dobet()
-  
-  if (betcalc < 0.01) then
-    print("Please set betcalc then restart the script")
-    stop() end
-  
-  if (lastBet.nonce>=maxbets) then
-    stop() end
-
+--
+  if (nextbet>balance) then
+    print("BALANCE NOT ENOUGH")
+    stop()
+  end
+  --
+  if (lastBet.nonce>0) and (lastBet.nonce<=50000) then
+    worststreak() end
+  --
+  if (lastBet.nonce==50000) then
+    if (maxstreakloses<=6) then betcalc = 27.364138 end
+    if (maxstreakloses==7) then betcalc = 7.3446286 end
+    if (maxstreakloses==8) then betcalc = 1.9714477 end
+    if (maxstreakloses==9) then betcalc = 0.5291766 end
+    if (maxstreakloses>=10) then
+        bethigh = true
+        stopnow = false
+        first = true
+        done = true
+        chance = 27.5
+        betcalc = 0.01023403
+        gan = 100000
+        base = balance * (betcalc / gan)
+        curbet = base * 3.7255
+        nextbet = base
+        maxstreakloses = 0
+        streakloses = 0
+        resetseed()
+        resetstats()
+      end
+    end
+  --    
+  if (lastBet.nonce>=200000) then
+        bethigh = true
+        stopnow = false
+        first = true
+        done = true
+        chance = 27.5
+        betcalc = 0.01023403
+        gan = 100000
+        base = balance * (betcalc / gan)
+        curbet = base * 3.7255
+        nextbet = base
+        maxstreakloses = 0
+        streakloses = 0
+        resetseed()
+        resetstats()
+  end
+  --
   base = balance * (betcalc / gan)
-
+  --
   if (first) then
     if(stopnow) then stop() end
     if !win then
@@ -41,7 +74,7 @@ function dobet()
       done = true
     end
   end
-
+  --
   if(!first and !done) then
     if(stopnow) then stop() end
     if win then
@@ -58,3 +91,15 @@ function dobet()
   end
   done = false
 end
+--
+function worststreak()
+  if !win then
+    streakloses=streakloses+1
+    if maxstreakloses<=streakloses then
+      maxstreakloses=streakloses
+    end
+  else
+    streakloses=0
+  end
+end
+--
