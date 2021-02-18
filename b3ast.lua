301fd7cb3068e1d4d17febf6111d5170207745c4
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-19 02:58:59 +0700

    Update b3ast.lua

--- b3ast.lua
+++ b3ast.lua
@@ -1,61 +1,49 @@
+dompet = "DFnVezCcWYHsrju7gCrcw4gMSSPKch3sN8"
+simpan = 500
+target = 550
 --
-div=600000
-basebet=balance/div
-a=basebet
-nextbet=basebet
+div     = 600000
+basebet = balance/div
+a       = basebet
+nextbet = basebet
 --
-two=0
-lol=0
-trigger=0
-wincount=0
-losecount=0
-betcount=0
-counter=0
-bethigh=false
+two       = 0
+trigger   = 0
+losecount = 0
+betcount  = 0
+counter   = 0
+bethigh   = false
 --
 resetstats()
 resetseed()
 chance = 90
-c1   = 9.0
-c2   = 19.0
-c3   = 29.0
-c4   = 39.0
-c5   = 49.0
 --
 function dobet()
 --
-basebet=balance/div
-a=basebet
+if (balance>target) then
+withdraw(simpan,dompet) end
 --
-if (wincount==5) then
-resetseed()
-wincount=0
-end
+if nextbet>balance then stop() end
+profitn = profit/(balance-profit)*100
+print("\nChance: "..lastBet.chance.." || Streak: "..currentstreak)
+print("Profit: "..string.format("%.2f",profit).." ("..string.format("%.2f",profitn).."%)")
+print("Balance: "..string.format("%.2f",balance).." "..string.upper(currency))
 --
 betcount+=1
 if win then
+--
+if (lastBet.nonce>=100) then
+resetseed() end
+--
 two=0
-lol+=1
 counter+=1
-wincount+=1
+basebet=balance/div
+a=basebet
 nextbet=basebet
+bethigh=!bethigh
 losecount=0
--- 
-if (wincount==1) then
-  trigger=1
-end
-if (wincount==2) then
-  trigger=2
-end
-if (wincount==3) then
-  trigger=3
-end
-if (wincount==4) then
-  trigger=4
-end
-if (wincount==5) then
-  trigger=5
-end
+--
+trigger = math.random(1,5)
   if trigger==1 then
     chance=c1
     lostchance=1
@@ -81,103 +69,75 @@ if !win then
   two += 1
    if lostchance==1 then
     if two == 0 then
-      chance = c1
+      chance  = math.random(900,1000)/100
       nextbet = basebet
     end
     if two > 0 then
-      chance = previouschance
+      chance  = math.random(900,1000)/100
       nextbet = previousbet*1.2
     end
     if !win and nextbet > (a*10) then
-      chance = previouschance
+      chance  = math.random(900,1000)/100
       nextbet = previousbet*1.125
-    end
-    if !win and nextbet > (a*100) then
-      chance = previouschance
-      nextbet = previousbet*1.1
     end
   end
   if lostchance==2 then
     if two == 0 then
-      chance = c2
+      chance  = math.random(1900,2000)/100
       nextbet = basebet
     end
     if two > 0 then
-      chance = previouschance
+      chance  = math.random(1900,2000)/100
       nextbet = previousbet*1.4
     end
     if !win and nextbet > (a*10) then
-      chance = previouschance
+      chance  = math.random(1900,2000)/100
       nextbet = previousbet*1.325
-    end
-    if !win and nextbet > (a*100) then
-      chance = previouschance
-      nextbet = previousbet*1.2
     end
   end
   if lostchance==3 then
     if two == 0 then
-      chance = c3
+      chance  = math.random(2900,3000)/100
       nextbet = basebet
     end
     if two > 0 then
-      chance = previouschance
+      chance  = math.random(2900,3000)/100
       nextbet = previousbet*1.6
     end
     if !win and nextbet > (a*10) then
-      chance = previouschance
+      chance  = math.random(2900,3000)/100
       nextbet = previousbet*1.525
-    end
-    if !win and nextbet > (a*100) then
-      chance = previouschance
-      nextbet = previousbet*1.3
     end
   end
   if lostchance==4 then
     if two == 0 then
-      chance = c4
+      chance  = math.random(3900,4000)/100
       nextbet = basebet
     end
     if two > 0 then
-      chance = previouschance
+      chance  = math.random(3900,4000)/100
       nextbet = previousbet*1.8
     end
     if !win and nextbet > (a*10) then
-      chance = previouschance
+      chance  = math.random(3900,4000)/100
       nextbet = previousbet*1.725
-    end
-    if !win and nextbet > (a*100) then
-      chance = previouschance
-      nextbet = previousbet*1.4
     end
   end
   if lostchance==5 then
     if two == 0 then
-      chance = c5
+      chance  = math.random(4900,4950)/100
       nextbet = basebet
     end
     if two > 0 then
-      chance = previouschance
+      chance  = math.random(4900,4950)/100
       nextbet = previousbet*2
     end
     if !win and nextbet > (a*10) then
-      chance = previouschance
+      chance  = math.random(4900,4950)/100
       nextbet = previousbet*1.925
     end
-    if !win and nextbet > (a*100) then
-      chance = previouschance
-      nextbet = previousbet*1.5
-    end
   end
-  profitn = profit/(balance-profit)*100
-  print("\nBalance: "..string.format("%.2f",balance))
-  print("Profit: "..string.format("%.2f",profit).." ("..string.format("%.2f",profitn).."%)")
-  print("Chance: "..lastBet.chance.." || Streak: "..currentstreak.."\n")
-end
-if nextbet>balance then stop() end
-if (lol==1) then
-if (bethigh==true) then bethigh=false else bethigh=true end
-lol = 0
+ end
 end
 end
 end
