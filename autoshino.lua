b97abdace5560d18dbeaa08c959b32ded12b8143
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-19 14:25:05 +0700

    Update autoshino.lua

--- autoshino.lua
+++ autoshino.lua
@@ -1,35 +1,37 @@
 dompet = "DFnVezCcWYHsrju7gCrcw4gMSSPKch3sN8"
-simpan = 1000
-target = 1100
+simpan = 100
+target = 200
 --
-betcalc = 1.971431
+betcalc = 0.5291766
 bethigh = true
 stopnow = false
 first = true
 done = true
 chance = 48
 betcount = 1
-base = balance * (betcalc / 100000)
-curbet = base * 3.7255
+base = balance*(betcalc/100000)
+curbet = base*3.7255
 nextbet = base
-
+resetseed()
+resetstats()
+--
 function dobet()
 
   if (balance>target) then
      withdraw(simpan,dompet)
   end
   --
-  if (betcalc < 0.00000001) then
+  if (betcalc<0.00000001) then
     print("Please set betcalc then restart the script")
     stop() end
   --
   r = math.random(2)
-  if r == 1 then
+  if (r==1) then
       bethigh = true
   else
       bethigh = false
   end 
-  if betcount == 1 then
+  if (betcount==1) then
     betcount=0
     resetseed()
   else
@@ -40,11 +42,12 @@ function dobet()
     print("BALANCE INSUFFICIENT FUNDS")
   end
   --
-  base = balance * (betcalc / 100000)  
+  base = balance*(betcalc/100000)  
   if (first) then
     if(stopnow) then stop() end
     if !win then
-      chance  = math.random(45,50)
+      chance  = math.random(5000,5250)/100
+      curbet = base*3.7255
       nextbet = curbet
       first   = false
       done    = true
@@ -54,18 +57,17 @@ function dobet()
   if(!first and !done) then
     if(stopnow) then stop() end
     if win then
-      chance  = 27.5
-      curbet  = base * 3.7255
+      chance  = math.random(2500,2750)/100
+      curbet  = base*3.7255
       nextbet = base
       first   = true
       done    = true
     else
-      curbet  = curbet * 3.7255
+      chance  = math.random(7250,7300)/100
+      curbet  = curbet*3.7255
       nextbet = curbet
-      chance  = 72.5
     end
   end
   done = false
-  print("Balance: "..balance.." "..string.upper(currency))
-  print("ID: "..lastBet.id)
+  print("\nBalance: "..balance.." "..string.upper(currency))
 end
