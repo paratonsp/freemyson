6a44e415b085bad31a4dea5be81b726516c64411
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-20 14:29:15 +0700

    Update viando.lua

--- viando.lua
+++ viando.lua
@@ -1,7 +1,12 @@
-gan      = 2500000
+gan    = 1000000
+dompet = "DFnVezCcWYHsrju7gCrcw4gMSSPKch3sN8"
+target = 101
+mtarget= 11
+simpan = balance-(target-mtarget)
+--
 nextbet  = balance/gan
 chance   = 65   --chance
-if_win   = 1.27 --if win
+if_win   = 1.16 --if win
 if_lose  = 1.40 --if lose
 streak   = 3    --streak
 bethigh  = false
@@ -11,14 +16,22 @@ resetstats()
 
 function dobet()
 --
-if (wincount==500) then
+if (balance>target) then
+    simpan = balance-(target-mtarget)
+    withdraw(simpan,dompet)
+    nextbet = balance/gan
+end
+--
+if nextbet>balance then stop() end
+--
+if (wincount==50) then
    resetseed()
    wincount = 0
 end
 --
 if win then
     if (currentstreak>=streak) then
-        chance  = math.random(6000,6500)/100
+        chance  = math.random(6400,6600)/100
         bethigh = !bethigh
         nextbet = balance/gan
     else
