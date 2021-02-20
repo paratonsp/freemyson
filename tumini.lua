418f65559a7bb77df1920cf0f68e4e4dfd597bd6
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-20 20:08:09 +0700

    Create tumini.lua

---
+++ tumini.lua
@@ -0,0 +1,39 @@
+ntarget = 11
+target  = 21
+simpan  = balance-(target-ntarget)
+dompet  = "DFnVezCcWYHsrju7gCrcw4gMSSPKch3sN8"
+--
+gan = 10000
+--
+bb = balance/gan
+iw = 1
+il = 1.6
+pr = 0
+--
+nextbet = bb
+chance = 19
+--
+function dobet()
+--
+if (balance>target) then
+    simpan = balance-(target-ntarget)
+    withdraw(simpan,dompet)
+    bb = balance/gan
+    nb = bb
+end
+--
+pf = profit
+pb = previousbet
+if win then
+    nb = pb * iw
+else
+    nb = pb * il
+end
+if pf > pr then
+    pr = pf
+    bb = balance/gan
+    nb = bb
+end
+nextbet = nb
+chance  = math.random(19,20)
+end
