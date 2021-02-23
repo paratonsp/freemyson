92811c7f208db68c853d9ce9bc7b2d8319ab3410
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-23 19:15:24 +0700

    Update jp.lua

--- jp.lua
+++ jp.lua
@@ -1,4 +1,4 @@
---
+--gan
 gan = 100000000
 --int
 base = balance/gan
@@ -8,18 +8,24 @@ wincount = 0
 losecount = 0
 bethigh = false
 w = 0
---
+--reset
 resetseed()
 resetstats()
 --dobet
 function dobet()
+
+--roll+
 roll+=1
+
 --resetseed/bethigh
-if (w==16) then
+if (w==5) then
     resetseed()
     bethigh = !bethigh
     w = 0
+    print(..balance
+    .." "..string.upper(currency)
 end
+
 --win
 if win then
     w+=1
@@ -32,17 +38,18 @@ if win then
     losecount+=1
     nextbet = previousbet*1.3
 end
+
 --win streak 2
 if (wincount==2) then
     wincount = 0
     nextbet = previousbet*1.2
     chance = 5
 end
+
+--losecount>10
 if (losecount>=10) then
     chance = 15.9
 end
-print(..balance
-.." "..string.upper(currency)
 end
 end
 end
