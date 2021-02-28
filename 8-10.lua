f5f72875ad90604c18d4ab5a56ba41e5e7ffb857
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-02-28 11:47:20 +0700

    Create 8-10.lua

---
+++ 8-10.lua
@@ -0,0 +1,21 @@
+div = 4000000
+--
+base = balance/div
+nextbet = base
+chance = 8.756789
+multi = 1.14
+cl = 8.756789
+cr = 10
+--
+resetstats()
+--
+function dobet()
+--
+chance = math.random(cl*100.0, cr*100.0)/100.0
+--
+if win then
+    nextbet = base
+else
+    nextbet = previousbet*multi
+end
+end
