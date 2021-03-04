ae0b96b2486fbc6515f01902b36eb424d7b0e549
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-03-05 04:12:06 +0700

    Update simple99.lua

--- simple99.lua
+++ simple99.lua
@@ -10,17 +10,14 @@ resetstats()
 --
 function dobet()
 --
-tt=100000000
-chance=math.random(9.9*tt,10*tt)/tt
---
 if win then
     base=balance/div
     nextbet=base
     bethigh=!bethigh
 else
     if (currentstreak<-5) then
+        sleep(1200)
         nextbet=previousbet*1.125
     end
 end
-if nextbet>(base*100) then sleep(1000) end
 end
