4c11a46d98ca5cf27adacc54f0ea4a38ecfd8fed
paratonsp <70687618+paratonsp@users.noreply.github.com>
2021-03-14 15:11:29 +0700

    Create redjump.lua

---
+++ redjump.lua
@@ -0,0 +1,26 @@
+div = 400 --200/400/800
+--
+chance = 49.5
+nextbet = 0.00000001
+bethigh = false
+--
+resetstats()
+function dobet()
+--
+if win then
+    nextbet = 0.00000001
+    bethigh = !bethigh
+end
+--
+if ! win then
+    nextbet = 0.00000001
+    if (currentstreak==-5) then
+        nextbet = balance/div
+    end
+    --
+    if (currentstreak<-5) then
+        nextbet = previousbet*2
+    end
+end
+--
+end
